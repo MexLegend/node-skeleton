b92f12a789c77f4d04916b6c6f8c60259278205a
Armando Lara <armandoskate2011@hotmail.com>
2024-02-04 10:11:27 -0600

    Update update.service.ts

--- src/services/example/update.service.ts
+++ src/services/example/update.service.ts
@@ -1,5 +1,5 @@
 import { internalServerError } from '@helpers/error.helper';
-import { IUpdateExample, EXAMPLE_ERROR_TYPE } from '@interfaces/example.interface';
+import { IUpdateExample, ExampleErrorType } from '@interfaces/example.interface';
 
 const main = async ({ id, email, name, password }: IUpdateExample) => {
 	try {
@@ -9,7 +9,7 @@ const main = async ({ id, email, name, password }: IUpdateExample) => {
 		// return response
 		return;
 	} catch (error) {
-		internalServerError<EXAMPLE_ERROR_TYPE>((<Error>error).message, 'UPDATE_EXAMPLE_ERROR');
+		internalServerError<ExampleErrorType>((<Error>error).message, 'UPDATE_EXAMPLE_ERROR');
 	}
 };
 
