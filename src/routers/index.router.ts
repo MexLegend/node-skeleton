28235cd2e4828929c62da62d44a22eec74fc579b
Armando Lara <armandoskate2011@hotmail.com>
2024-02-04 14:52:08 -0600

    Update index.router.ts

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -1,30 +1,10 @@
 import { Router } from 'express';
-import fs from 'fs';
-import path from 'path';
+import exampleRouter from './example.router';
 import { notFoundError } from '@helpers/error.helper';
 
 const router = Router();
 
-const pathRouter = `${__dirname}`;
-
-const removeExtension = (fileName: string) => {
-    return fileName.split('.').shift();
-}
-
-fs.readdirSync(pathRouter).filter(async file => {
-    const fileWithoutExtension = removeExtension(file);
-
-    if (fileWithoutExtension && !fileWithoutExtension.includes('index')) {
-        try {
-            const routePath = path.join(pathRouter, fileWithoutExtension + '.router');
-            const module = await import(routePath);
-            router.use(`/${fileWithoutExtension}`, module.default);
-        } catch (error) {
-            notFoundError((error as Error).message, 'LOAD_ROUTE_ERROR');
-
-        }
-    }
-});
+router.use('/example', exampleRouter);
 
 router.get('*', () => {
     notFoundError('Route Not found', 'ROUTE_NOT_FOUND');
