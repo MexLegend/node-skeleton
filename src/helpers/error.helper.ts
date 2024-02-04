e3f06cf6a44a9d4481264998910feffd3a533238
Armando Lara <armandoskate2011@hotmail.com>
2024-02-04 10:00:39 -0600

    Update error.helper.ts

--- src/helpers/error.helper.ts
+++ src/helpers/error.helper.ts
@@ -1,4 +1,4 @@
-import { ERROR_TYPE, IError } from "@interfaces/error.interface";
+import { ErrorType, IError } from "@interfaces/error.interface";
 
 export class CustomError<T> extends Error implements IError<T> {
     code: T;
@@ -12,32 +12,32 @@ export class CustomError<T> extends Error implements IError<T> {
     }
 }
 
-const conflictError = <T = ERROR_TYPE>(msg: string, code?: T) => {
+const conflictError = <T = ErrorType>(msg: string, code?: T) => {
     const err = new CustomError(msg, code || 'CONFLICT', 409);
     throw err;
 };
 
-const notFoundError = <T = ERROR_TYPE>(msg: string, code?: T) => {
+const notFoundError = <T = ErrorType>(msg: string, code?: T) => {
     const err = new CustomError(msg, code || 'NOT_FOUND', 404);
     throw err;
 };
 
-const notAuthorizedError = <T = ERROR_TYPE>(msg: string, code?: T) => {
+const notAuthorizedError = <T = ErrorType>(msg: string, code?: T) => {
     const err = new CustomError(msg, code || 'UNAUTHORIZED', 401);
     throw err;
 };
 
-const forbiddenError = <T = ERROR_TYPE>(msg: string, code?: T) => {
+const forbiddenError = <T = ErrorType>(msg: string, code?: T) => {
     const err = new CustomError(msg, code || 'FORBIDDEN', 403);
     throw err;
 };
 
-const internalServerError = <T = ERROR_TYPE>(msg: string, code?: T) => {
+const internalServerError = <T = ErrorType>(msg: string, code?: T) => {
     const err = new CustomError(msg, code || 'INTERNAL_ERROR', 500);
     throw err;
 };
 
-const badRequestError = <T = ERROR_TYPE>(msg: string, code?: T) => {
+const badRequestError = <T = ErrorType>(msg: string, code?: T) => {
     const err = new CustomError(msg, code || 'BAD_REQUEST_ERROR', 400);
     throw err;
 };
