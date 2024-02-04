8b52fce19257d90be3c93c1c24290cdb1e898722
Armando Lara <armandoskate2011@hotmail.com>
2024-02-04 11:13:07 -0600

    Update deleteOne.controller.ts

--- src/controllers/example/deleteOne.controller.ts
+++ src/controllers/example/deleteOne.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { deleteOne } from '@services/example/index.service';
 
 import { validateHelper } from '@helpers/validate.helper';
 import schema from '@validations/shared/getOneById.schema';
@@ -11,6 +12,8 @@ const main = async (req: Request, res: Response, next: NextFunction) => {
         // Validate schema
         const { id } = req.params;
         await validateHelper<IIdentifier>(schema, { id });
+
+        await deleteOne(id);
 
         // Return response
         res.send('Example deleted successfully');
