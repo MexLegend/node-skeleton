3c1e383cd0ba777338ec507d0fcead57808433f7
Armando Lara <armandoskate2011@hotmail.com>
2024-02-04 10:00:50 -0600

    Update error.interface.ts

--- src/interfaces/error.interface.ts
+++ src/interfaces/error.interface.ts
@@ -1,4 +1,4 @@
-export type Error_Type =
+export type ErrorType =
     | 'CONFLICT'
     | 'NOT_FOUND'
     | 'UNAUTHORIZED'
@@ -6,7 +6,7 @@ export type Error_Type =
     | 'INTERNAL_ERROR'
     | 'BAD_REQUEST_ERROR';
 
-export interface IError<T = Error_Type> {
+export interface IError<T = ErrorType> {
     readonly code: T;
     readonly httpStatus: number;
     readonly message: string;
